16fbbc3d55c182a43005c4d0ff665fb4d76358fc
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2024-08-06 03:42:48 +0200

    added Forms to create USER as Tenant

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,5 +1,64 @@
 from django.contrib import admin
 from tenants.models import Tenant
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(Tenant)
+class TenantCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Tenant
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(TenantCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class TenantChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Tenant
+        fields = ('email', 'password', 'first_name', 'last_name', 'phone_number', 'flat', 'is_active', 'is_staff', 'is_superuser')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+class TenantAdmin(UserAdmin):
+    form = TenantChangeForm
+    add_form = TenantCreationForm
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'flat')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'phone_number', 'flat', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(Tenant, TenantAdmin)
